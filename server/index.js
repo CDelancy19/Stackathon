0348cd11acd382722a2c0576e4e4eafbe51edcee
Christina Delancy <cdelancy11@yahoo.com>
2022-01-31 22:52:39 -0500

    Removed database seding

--- server/index.js
+++ server/index.js
@@ -1,7 +1,6 @@
 const { db } = require('./db');
 const PORT = process.env.PORT || 8080;
 const app = require('./app');
-const seed = require('../script/seed');
 // Require google from googleapis package.
 const { google } = require('googleapis');
 
@@ -287,11 +286,6 @@ getWomensEvent();
 
 const init = async () => {
 	try {
-		if (process.env.SEED === 'true') {
-			await seed();
-		} else {
-			await db.sync();
-		}
 		// start listening (and create a 'server' object representing our server)
 		app.listen(PORT, () => console.log(`Mixing it up on port ${PORT}`));
 	} catch (ex) {
