eb8bd19631913e10c4a4498f2cdebd8b4776844c
Christina Delancy <cdelancy11@yahoo.com>
2022-01-31 23:13:08 -0500

    made the home page the landing page

--- client/Routes.js
+++ client/Routes.js
@@ -12,7 +12,7 @@ class Routes extends Component {
 		return (
 			<div>
 				<Switch>
-					<Route path="/home" component={Home} />
+					<Route exact path="/" component={Home} />
 					<Route path="/calendar" component={Calendar} />
 					<Route path="/next" component={Next} />
 				</Switch>
--- client/components/Navbar.js
+++ client/components/Navbar.js
@@ -6,11 +6,11 @@ const Navibar = () => (
 	<div>
 		<Navbar bg="light" expand="lg">
 			<Container>
-				<Navbar.Brand href="#home">Tennis Events</Navbar.Brand>
+				<Navbar.Brand href="/">Tennis Events</Navbar.Brand>
 				<Navbar.Toggle aria-controls="basic-navbar-nav" />
 				<Navbar.Collapse id="basic-navbar-nav">
 					<Nav className="me-auto">
-						<Nav.Link href="/home">Home</Nav.Link>
+						<Nav.Link href="/">Home</Nav.Link>
 						<Nav.Link href="/calendar">Calendar</Nav.Link>
 						<Nav.Link href="/next">What's Next?</Nav.Link>
 					</Nav>
